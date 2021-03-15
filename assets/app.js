8f62a05d5e6c9bb094144791ee199be0776b7d39
mvchn <movchan@gmail.com>
2021-03-15 17:55:14 +0200

    add frontend features

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,22 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+//import './styles/app.css';
+
+import './styles/global.scss';
 
 // start the Stimulus application
-import './bootstrap';
+//import './bootstrap';
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
