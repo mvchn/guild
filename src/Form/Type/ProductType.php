a9b3196f114469ffb86652276971654c079b96b4
mvchn <movchan@gmail.com>
2021-03-27 18:55:56 +0200

    add products

---
+++ src/Form/Type/ProductType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProductType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+        ;
+    }
+}
