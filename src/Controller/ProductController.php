9381b7906dc6ca0928c18dc913062bef45efb5f9
mvchn <movchan@gmail.com>
2021-04-18 16:43:24 +0300

    add product features and test

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,22 +2,55 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
 use App\Entity\Product;
+use App\Form\OrderType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/product/{id}", name="product_show", requirements={"id"="\d+"})
+     * @Route("/product/{id}", methods={"GET"}, name="product_show", requirements={"id"="\d+"})
      * @ParamConverter("product", class="App:Product")
      */
     public function show(Product $product): Response
     {
         return $this->render('product/show_order.html.twig', [
             'product' => $product,
+        ]);
+    }
+
+    /**
+     * @Route("/product/{id}/order", methods={"GET", "POST"}, name="product_order", requirements={"id"="\d+"})
+     * @ParamConverter("product", class="App:Product")
+     */
+    public function order(Product $product, Request $request): Response
+    {
+        $order = new Order();
+        $order->addProduct($product);
+
+        $form = $this->createForm(OrderType::class, $order);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->addFlash(
+                'success',
+                'Order created'
+            );
+            $this->getDoctrine()->getManager()->persist($order);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('product_order', ['id' => $product->getId()]);
+        }
+
+        return $this->render('product/order.html.twig', [
+            'product' => $product,
+            'form' => $form->createView()
         ]);
     }
 }
--- tests/Controller/ProductControllerTest.php
+++ tests/Controller/ProductControllerTest.php
@@ -2,17 +2,40 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\Order;
+use App\Repository\OrderRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ProductControllerTest extends WebTestCase
 {
-    public function testSuccess()
+    public function testShowSuccess()
     {
         $client = static::createClient();
 
         $client->request('GET', '/product/1');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testAddOrderSuccess()
+    {
+        $client = static::createClient();
+        $orderRepository = static::$container->get(OrderRepository::class);
+
+        $client->request('GET', '/product/1/order');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $client->submitForm('Create', [
+            'order[name]' => 'name',
+            'order[email]' => 'movchan@gmail.com',
+        ]);
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+
+        $order = $orderRepository->findOneBy(['name' => 'name', 'email' => 'movchan@gmail.com']);
+
+        $this->assertInstanceOf(Order::class, $order);
     }
 
 
